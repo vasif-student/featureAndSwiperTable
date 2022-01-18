8896de972971009b420e13a67a476ea7acd2db83
vasif-student <vasifah@code.edu.az>
2022-01-19 00:47:15 +0400

    createFeatureAndSwiperTable

---
+++ Hotel-App/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Hotel_App.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_App.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Banner Banners { get; set; }
+        public BannerImage BannerImages { get; set; }
+        public List<Feature> Features { get; set; }
+        public List<Swiper> Swipers { get; set; }
+    }
+}
